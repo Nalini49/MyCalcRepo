a6ad086e99ebef5ad7d97708ae69b6c451a311b4
Nalini49 <72907690+Nalini49@users.noreply.github.com>
2020-10-20 10:56:57 +0530

    Update Calc.java

--- Calc.java
+++ Calc.java
@@ -7,6 +7,6 @@ public static int add(int a,int b){
  }
 public static void main(String []args){
 System.out.println(add(10,20));
-  System.out.println(add(50,40));
+  System.out.println(sub(50,40));
 }
 }
